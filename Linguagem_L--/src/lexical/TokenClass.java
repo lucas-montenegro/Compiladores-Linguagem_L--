b72e2086b5bc194984daf481da864e6979a6bd2c
Lucas Montenegro Andrade Assunção <43447533+lucas-montenegro@users.noreply.github.com>
2021-04-01 12:11:24 -0300

    Delete Linguagem_L-- directory

--- Linguagem_L--/src/lexical/TokenClass.java
+++
@@ -1,5 +0,0 @@
-package lexical;
-
-public enum TokenClass {
-     unknown, main, id, typeInt, typeFloat, typeChar, typeBool, typeString, constInt, constFloat, constBool, constChar,             constString, optAdd, optPow, optMult, optDiv, optSub, optMod, optAttib, optLess, optLessEq, optGreat, optGreatEq, optEq,       optNotEq, optAnd, optOr, optNot, optConcat, condIf, condElsif, condElse, loopWhile, loopFor, read, write, separator,           paramInit, paramEnd, arrInit, arrEnd, comment, funcDef, funcReturn, funcInit, funcEnd, sentenEnd, endFile
-}
